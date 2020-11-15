e2227868b7b1c4a1afd6076ab833b68c5b8af2fc
SAITO Tomomi <t-saito@project-flora.net>
2020-11-15 20:28:57 +0900

    [Feature] Add empty message support

--- Sources/Model/Settings.swift
+++ Sources/Model/Settings.swift
@@ -191,6 +191,20 @@ import Photos
         /// Allow the user to dismiss the image picker by swiping down
         public lazy var allowSwipe = false
     }
+    
+    public class Message : NSObject {
+        /// Messages for empty
+        public lazy var emptyMessage = "Item are empty"
+        public lazy var messageAttributes : [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor: UIColor.systemPrimaryTextColor,
+            .paragraphStyle: {
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.alignment = .center
+                return paragraphStyle
+            }()
+        ]
+    }
 
     /// Theme settings
     public lazy var theme = Theme()
@@ -209,4 +223,7 @@ import Photos
 
     /// Preview options
     public lazy var preview = Preview()
+    
+    /// Message settings
+    public lazy var message = Message()
 }
